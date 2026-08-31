05820c506bc2d36afbb9c93e0bcaa03d5895f269
Pooja Yeola <poojayeola42@gmail.com>
2026-08-31 16:34:17 +0530

    31 august first

--- src/components/header/AppHeaderDropdownMssg.js
+++ src/components/header/AppHeaderDropdownMssg.js
@@ -1,21 +1,10 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
-import {
- 
-  CDropdown,
-
-} from '@coreui/react-pro'
-import CIcon from '@coreui/icons-react'
-import { cilEnvelopeOpen } from '@coreui/icons'
 
 const AppHeaderDropdownMssg = () => {
-  const { t } = useTranslation()
-  const itemsCount = 4
   return (
-    <CDropdown variant="nav-item" alignment="end">
-
-       
-    </CDropdown>
+    <div>
+      
+    </div>
   )
 }
 
